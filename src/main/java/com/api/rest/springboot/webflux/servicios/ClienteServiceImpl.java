eac014a3a74a122ed4333939ceeca7ec9ff3c44e
Calapzu <yamsoncalapzu@gmail.com>
2023-01-20 23:04:29 -0500

    Se crea el proyecto con el documento CLIENTE, el repositorio, los servicios y se implementa el servicio

---
+++ src/main/java/com/api/rest/springboot/webflux/servicios/ClienteServiceImpl.java
@@ -0,0 +1,33 @@
+package com.api.rest.springboot.webflux.servicios;
+
+import com.api.rest.springboot.webflux.documentos.Cliente;
+import com.api.rest.springboot.webflux.repositorios.ClienteDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class ClienteServiceImpl implements ClienteService{
+
+    @Autowired
+    private ClienteDAO clienteDAO;
+
+    @Override
+    public Flux<Cliente> findAll() {
+        return clienteDAO.findAll();
+    }
+
+    @Override
+    public Mono<Cliente> findById(String id) {
+        return clienteDAO.findById(id);
+    }
+
+    @Override
+    public Mono<Cliente> save(Cliente cliente) {
+        return clienteDAO.save(cliente);
+    }
+
+    @Override
+    public Mono<Void> delete(Cliente cliente) {
+        return clienteDAO.delete(cliente);
+    }
+}
